c67aee5b270d952337642b8dff158daaeb192ad9
zadiki ochola hassan <zadikiochola@gmail.com>
2024-07-13 19:25:12 +0300

    initial comit

---
+++ src/main/java/com/pos/iduka/IdukaApplication.java
@@ -0,0 +1,14 @@
+package com.pos.iduka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdukaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IdukaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pos/iduka/IdukaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.iduka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdukaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
