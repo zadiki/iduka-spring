03a1c0a7b649e075a47fe7fe8b469958616295d7
zadiki ochola hassan <zadikiochola@gmail.com>
2024-07-16 11:54:56 +0300

    added json ignore for password field

--- src/main/java/com/pos/iduka/model/UserInfo.java
+++ src/main/java/com/pos/iduka/model/UserInfo.java
@@ -3,6 +3,9 @@ package com.pos.iduka.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,10 +20,20 @@ public class UserInfo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "Tutorial Id", example = "123")
     private Long id;
-    private String name;
+
+    @Schema(description = "user full name", example = "zadiki hassan")
+    @NotBlank(message = "Please provide a name")
+    private  String name;
+
+    @Schema(description = "user email address", example = "zadiki@gmail.com")
+    @Email(message = "Please provide a valid email address")
     @Column(unique = true)
     private String email;
+
+    @Schema(description = "user secured password with numbers and letters minimum 4 character max 10 alphanumeric", example = "zadiki832")
+    @Size(min = 8,max = 10, message = "Password must be at least 4 and max 10 characters long")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String roles;
+
 }
